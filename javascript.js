10a48af4a34246a2dcc2e4b28e6fa88082a71a77
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 15:52:40 -0500

    Add rock paper scissors logic and simulate a round

--- javascript.js
+++ javascript.js
@@ -27,5 +27,32 @@ function getHumanChoice(){
 }
 
 function playRound(humanChoice, computerChoice){
-    
+    console.log(humanChoice);
+    console.log(computerChoice);
+    if(humanChoice === computerChoice){
+        console.log("Its a tie! Try again.");
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You win! Rock beats Scissors.");
+    }
+    else if(humanChoice === "scissors" && computerChoice === "rock"){
+        console.log("You lose! Rock beats Scissors.");
+    }
+    else if(humanChoice === "rock" && computerChoice === "paper"){
+        console.log("You lose! Paper beats Rock.");
+    }
+    else if(humanChoice === "paper" && computerChoice === "rock"){
+        console.log("You win! Paper beats Rock.");
+    }
+    else if(humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("You win! Scissors beats Paper.");
+    }
+    else if(humanChoice === "paper" && computerChoice === "scissors"){
+        console.log("You lose! Scissors beats Paper.");
+    }
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
