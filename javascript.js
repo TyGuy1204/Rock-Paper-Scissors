42a2c3cd4b0c191192ea75022dabb1b6848c6454
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 15:31:44 -0500

    Create function to generate computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+function getComputerChoice(){
+    let choice  = Math.floor(Math.random() * 3)+1;
+    if(choice == 1){
+        return "Rock";
+    }
+    else if(choice == 2){
+        return "Paper";
+    }
+    else if(choice == 3){
+        return "Scissors";
+    }
+}
